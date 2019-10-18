671db0efce834a1e2c773b7f1997978a970efc94
wagner.nasc <35998576+wagnernascimentos1@users.noreply.github.com>
2019-10-18 15:25:55 -0300

    Create Imposto.java

---
+++ Imposto.java
@@ -0,0 +1,59 @@
+public class Imposto{
+    private String Nome;
+    private String Descricao;
+    private double Taxa;
+    private double Renda;
+    
+public class Imposto(String Nome, String Descricao, double Taxa, double Renda){
+    this.Nome = nome;
+    this.Descricao = descricao;
+    this.Taxa = taxa;
+    this.Renda = renda;
+}
+
+public String getNome() {
+    return nome;
+}
+
+public void setNome (String nome){
+    this.nome = nome;
+}
+
+public String getDescricao(){
+    return Descricao;
+}
+
+public void setDescricao (String Descricao){
+    this.Descricao = Descricao;
+}
+
+public double getTaxa(){
+    return Taxa;
+}
+
+public void setTaxa(double Taxa){
+    this.Taxa = taxa;
+}
+
+public double getRenda(){
+    return Renda;
+}
+
+public void setRenda(double Renda){
+    this.Renda = renda;
+}
+
+public double CalcularIR{
+    IR = (taxa * renda)
+        return IR;
+}
+
+public double CalcularIPTU{
+    IPTU = (taxa * valorCarro)
+        return IPTU;
+}
+
+
+
+}
+
